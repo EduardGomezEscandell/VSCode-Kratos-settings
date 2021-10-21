0d6e2a5884aca5df2a4a2efeeb6bc9652b844fb8
Eduard Gómez <eduard.gomez.escandell@gmail.com>
2021-10-21 10:10:00 +0200

    Updated

--- build/configure.sh
+++ build/configure.sh
@@ -3,27 +3,28 @@ add_app () {
     export KRATOS_APPLICATIONS="${KRATOS_APPLICATIONS}$1;"
 }
 
+
 # Set compiler
 export CC=gcc
 export CXX=g++
 
 # Set variables
-export KRATOS_SOURCE=${KRATOS_SOURCE:-"$( cd "$(dirname "$0")" ; pwd -P )"/..}
-export KRATOS_BUILD=${KRATOS_BUILD:-"${KRATOS_SOURCE}/build"}
-export KRATOS_APP_DIR=${KRATOS_APP_DIR:-"${KRATOS_SOURCE}/applications"}
+export KRATOS_SOURCE=$KRATOS
+export KRATOS_BUILD="${KRATOS_SOURCE}/build"
+export KRATOS_APP_DIR="${KRATOS_SOURCE}/applications"
+export KRATOS_INSTALL_PYTHON_USING_LINKS=ON
 
 # Set basic configuration
 export KRATOS_BUILD_TYPE=${KRATOS_BUILD_TYPE:-"Release"}
 export PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE:-"/usr/bin/python3"}
-export KRATOS_INSTALL_PYTHON_USING_LINKS="ON"
 export KRATOS_USE_MPI=${KRATOS_USE_MPI:-"OFF"}
 
 # Set applications to compile
 export KRATOS_APPLICATIONS=
-for var in "$@"
-do
-    add_app ${KRATOS_APP_DIR}/$var
-done
+add_app "${KRATOS_APP_DIR}/LinearSolversApplication"
+add_app "${KRATOS_APP_DIR}/StructuralMechanicsApplication"
+add_app "${KRATOS_APP_DIR}/FluidDynamicsApplication"
+add_app "${KRATOS_APP_DIR}/CompressiblePotentialFlowApplication"
 
 # Clean
 clear
@@ -38,4 +39,4 @@ cmake -H"${KRATOS_SOURCE}" -B"${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}"     \
 -DUSE_MPI=${KRATOS_USE_MPI}
 
 # Build
-cmake --build "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}" --target install -- -j$(nproc)
+cmake --build "${KRATOS_BUILD}/${KRATOS_BUILD_TYPE}" --target install -- -j4
